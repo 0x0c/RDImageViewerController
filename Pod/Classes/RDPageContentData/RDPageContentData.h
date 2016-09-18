7f77f602bcef9385d20695fafbbbbd53674600d7
Akira Matsuda <akira.matsuda@me.com>
2016-09-18 22:58:01 +0900

    Bug fix

--- Pod/Classes/RDPageContentData/RDPageContentData.h
+++ Pod/Classes/RDPageContentData/RDPageContentData.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
 @protocol RDPageContentDataDelegate <NSObject>
-
+@required
 + (UIView * _Nonnull)contentViewWithFrame:(CGRect)frame;
 - (void)preload;
 - (void)stopPreloading;
