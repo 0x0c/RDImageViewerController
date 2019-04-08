e9e0e8b06dd968348ee0dc33ee1a708f0e1ce3b5
Akira <akira.matsuda@me.com>
2019-04-09 01:25:36 +0900

    Update demo app

--- Example/RDImageViewerController/ViewController.swift
+++ Example/RDImageViewerController/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 import RDImageViewerController
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var showSlider: UISwitch!
+    @IBOutlet weak var showHud: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,47 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setToolbarHidden(true, animated: true)
+    }
+    
+    func contents() -> [RDPageContentData] {
+        var contents = [RDPageContentData]()
+        for i in 0..<12 {
+            let data = RDImageContentData(imageName: "\(i + 1).JPG")
+            contents.append(data)
+        }
+        
+        return contents
+    }
 
+    @IBAction func image(_ sender: Any) {
+        let viewController = RDImageViewerController(contents: contents(), direction: .left)
+        viewController.showSlider = showSlider.isOn
+        viewController.showPageNumberHud = showHud.isOn
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    @IBAction func showScrollView(_ sender: Any) {
+        
+    }
+    
+    @IBAction func showView(_ sender: Any) {
+        
+    }
+    
+    @IBAction func aspectFit(_ sender: Any) {
+        
+    }
+    
+    @IBAction func displayFit(_ sender: Any) {
+        
+    }
+    
+    @IBAction func viewAndImage(_ sender: Any) {
+        
+    }
 }
 
