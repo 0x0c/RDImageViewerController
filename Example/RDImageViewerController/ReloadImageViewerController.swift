4a67998d4d51d7d3d463732dee084abbec747488
Akira <akira.matsuda@me.com>
2019-04-13 23:09:16 +0900

    Add button to push an another viewer.

--- Example/RDImageViewerController/ReloadImageViewerController.swift
+++ Example/RDImageViewerController/ReloadImageViewerController.swift
@@ -12,7 +12,8 @@ import RDImageViewerController
 class ReloadImageViewerController: RDImageViewerController {
     override init(contents: [RDPageContentData], direction: RDPagingView.ForwardDirection) {
         super.init(contents: contents, direction: direction)
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(reload))
+        let items = [UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(reload)), UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(push))]
+        navigationItem.setRightBarButtonItems(items, animated: true)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -24,5 +25,13 @@ class ReloadImageViewerController: RDImageViewerController {
         let contents = ContentsFactory.randomContents()
         print(contents)
         update(contents: contents)
+    }
+    
+    @objc func push() {
+        let viewController = ReloadImageViewerController(contents: ContentsFactory.scrollContents(), direction: pagingView.direction)
+        viewController.showSlider = showSlider
+        viewController.showPageNumberHud = showPageNumberHud
+        viewController.title = "View and Image"
+        navigationController?.pushViewController(viewController, animated: true)
     }
 }
