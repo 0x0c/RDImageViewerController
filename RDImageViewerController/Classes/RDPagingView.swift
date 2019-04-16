39497ec446160a71b5e56049148d79a7487d0f36
Akira <akira.matsuda@me.com>
2019-04-17 01:40:23 +0900

    Call pagingView(pagingView: RDPagingView, willChangeIndexTo index: Int) only page index will change.

--- RDImageViewerController/Classes/RDPagingView.swift
+++ RDImageViewerController/Classes/RDPagingView.swift
@@ -142,13 +142,18 @@ extension RDPagingView : UIScrollViewDelegate
         
         let to = Int(position + 0.5)
         if let pagingDelegate = pagingDelegate {
-            if isLegacyLayoutSystem {
-                pagingDelegate.pagingView?(pagingView: self, willChangeIndexTo: numberOfPages - to - 1)
-            }
-            else {
-                pagingDelegate.pagingView?(pagingView: self, willChangeIndexTo: to)
+            if previousIndex != to {
+                if isLegacyLayoutSystem {
+                    pagingDelegate.pagingView?(pagingView: self, willChangeIndexTo: numberOfPages - to - 1)
+                }
+                else {
+                    pagingDelegate.pagingView?(pagingView: self, willChangeIndexTo: to)
+                }
             }
             pagingDelegate.pagingView?(pagingView: self, didScrollToPosition: position)
+        }
+        if previousIndex != to {
+            previousIndex = to
         }
         _currentPageIndex = to
     }
