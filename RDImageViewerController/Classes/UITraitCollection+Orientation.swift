e13e704b9be9fae01c00b3867d89185ded9404e3
Akira <akira.matsuda@me.com>
2020-01-22 03:14:21 +0900

    Fix compile error

---
+++ RDImageViewerController/Classes/UITraitCollection+Orientation.swift
@@ -0,0 +1,32 @@
+//
+//  UITraitCollection+Orientation.swift
+//  Pods-RDImageViewerController_Example
+//
+//  Created by Akira Matsuda on 2020/01/22.
+//
+
+import Foundation
+
+extension UIApplication {
+    public func currentWindow() -> UIWindow? {
+        if #available(iOS 13.0, *) {
+            return connectedScenes.filter({$0.activationState == .foregroundActive}).map({$0 as? UIWindowScene}).compactMap({$0}).first?.windows.filter({$0.isKeyWindow}).first
+        }
+        else {
+            return UIApplication.shared.keyWindow
+        }
+    }
+}
+
+extension UITraitCollection {
+    public func isLandscape() -> Bool {
+        if #available(iOS 13, *)  {
+            if let window = UIApplication.shared.currentWindow(),
+                let windowScene = window.windowScene {
+                return windowScene.interfaceOrientation.isLandscape
+            }
+            return false
+        }
+        return UIApplication.shared.statusBarOrientation.isLandscape
+    }
+}
